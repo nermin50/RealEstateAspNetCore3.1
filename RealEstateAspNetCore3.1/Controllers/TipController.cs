a070619019a1dbadf1a192d6d839fad3e007439c
nermin50 <nerminsaar@gmail.com>
2021-08-20 23:16:30 +0300

    type Controller tamamen yapıldı

---
+++ RealEstateAspNetCore3.1/Controllers/TipController.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using RealEstateAspNetCore3._1.Models;
+
+namespace RealEstateAspNetCore3._1.Controllers
+{
+    public class TipController : Controller
+    {
+        private readonly DataContext _context;
+
+        public TipController(DataContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Tip
+        public async Task<IActionResult> Index()
+        {
+            var dataContext = _context.Tips.Include(t => t.Status);
+            return View(await dataContext.ToListAsync());
+        }
+
+        // GET: Tip/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var tip = await _context.Tips
+                .Include(t => t.Status)
+                .FirstOrDefaultAsync(m => m.TypeId == id);
+            if (tip == null)
+            {
+                return NotFound();
+            }
+
+            return View(tip);
+        }
+
+        // GET: Tip/Create
+        public IActionResult Create()
+        {
+            ViewData["StatusId"] = new SelectList(_context.Status, "StatusId", "StatusName");
+            return View();
+        }
+
+        // POST: Tip/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("TypeId,TypeName,StatusId")] Tip tip)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(tip);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["StatusId"] = new SelectList(_context.Status, "StatusId", "StatusId", tip.StatusId);
+            return View(tip);
+        }
+
+        // GET: Tip/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var tip = await _context.Tips.FindAsync(id);
+            if (tip == null)
+            {
+                return NotFound();
+            }
+            ViewData["StatusId"] = new SelectList(_context.Status, "StatusId", "StatusName", tip.StatusId);
+            return View(tip);
+        }
+
+        // POST: Tip/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("TypeId,TypeName,StatusId")] Tip tip)
+        {
+            if (id != tip.TypeId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(tip);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TipExists(tip.TypeId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["StatusId"] = new SelectList(_context.Status, "StatusId", "StatusId", tip.StatusId);
+            return View(tip);
+        }
+
+        // GET: Tip/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var tip = await _context.Tips
+                .Include(t => t.Status)
+                .FirstOrDefaultAsync(m => m.TypeId == id);
+            if (tip == null)
+            {
+                return NotFound();
+            }
+
+            return View(tip);
+        }
+
+        // POST: Tip/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var tip = await _context.Tips.FindAsync(id);
+            _context.Tips.Remove(tip);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool TipExists(int id)
+        {
+            return _context.Tips.Any(e => e.TypeId == id);
+        }
+    }
+}
