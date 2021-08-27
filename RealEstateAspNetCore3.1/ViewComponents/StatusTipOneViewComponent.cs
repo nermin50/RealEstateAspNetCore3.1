991963806d9e488a068ac62dfd8b024f9a5c89fe
nermin50 <nerminsaar@gmail.com>
2021-08-28 00:31:43 +0300

    Component View To Type Table List it

---
+++ RealEstateAspNetCore3.1/ViewComponents/StatusTipOneViewComponent.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RealEstateAspNetCore3._1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RealEstateAspNetCore3._1.ViewComponents
+{
+    public class StatusTipOneViewComponent : ViewComponent
+    {
+        private readonly DataContext _db;
+
+        public StatusTipOneViewComponent(DataContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var tiplist = _db.Tips.Where(i => i.StatusId == 1).ToList();
+            return View(tiplist);
+
+        }
+
+    }
+}
---
+++ RealEstateAspNetCore3.1/ViewComponents/StatusTipTwoViewComponent.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RealEstateAspNetCore3._1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RealEstateAspNetCore3._1.ViewComponents
+{
+    public class StatusTipTwoViewComponent : ViewComponent
+    {
+        private readonly DataContext _db;
+
+        public StatusTipTwoViewComponent(DataContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var tiplist = _db.Tips.Where(i => i.StatusId == 2).ToList();
+            return View(tiplist);
+
+        }
+    }
+}
