20f1d6641a8b7a2ec9d39a2be9d72e04b341175d
nermin50 <nerminsaar@gmail.com>
2021-08-28 00:13:58 +0300

    add Component View For  Status Name  in Tip Table

---
+++ RealEstateAspNetCore3.1/ViewComponents/StatusNameOneViewComponent.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RealEstateAspNetCore3._1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RealEstateAspNetCore3._1.ViewComponents
+{
+    public class StatusNameOneViewComponent : ViewComponent
+    {
+        private readonly DataContext _db;
+
+        public StatusNameOneViewComponent(DataContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var statusname1 = _db.Tips.Where(i => i.StatusId == 1).Include(m => m.Status).FirstOrDefault();
+            return View(statusname1);
+
+        }
+    }
+}
---
+++ RealEstateAspNetCore3.1/ViewComponents/StatusNameTwoViewComponent.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RealEstateAspNetCore3._1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RealEstateAspNetCore3._1.ViewComponents
+{
+    public class StatusNameTwoViewComponent : ViewComponent
+    {
+        private readonly DataContext _db;
+
+        public StatusNameTwoViewComponent(DataContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            //Satlık veriyi çağıryor 
+            var statusname1 = _db.Tips.Where(i => i.StatusId == 2).Include(m => m.Status).FirstOrDefault();
+
+            return View(statusname1);
+
+        }
+    }
+}
