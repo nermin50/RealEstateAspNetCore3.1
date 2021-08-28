1d19f7355198d8c9edace59a12593033b69983aa
nermin50 <nerminsaar@gmail.com>
2021-08-29 00:12:31 +0300

    Paginations

--- RealEstateAspNetCore3.1/ViewComponents/AdvListViewComponent.cs
+++ RealEstateAspNetCore3.1/ViewComponents/AdvListViewComponent.cs
@@ -1,10 +1,12 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+
 using RealEstateAspNetCore3._1.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using X.PagedList;
 
 namespace RealEstateAspNetCore3._1.ViewComponents
 {
@@ -16,17 +18,15 @@ namespace RealEstateAspNetCore3._1.ViewComponents
         {
             _db = db;
         }
-        public async Task<IViewComponentResult> InvokeAsync()
+        public async Task<IViewComponentResult> InvokeAsync(int page = 1)
         {
-
             var imgs = _db.advPhotos.ToList();
             ViewBag.imgs = imgs;
 
             var adv = _db.advertisements.Include(l => l.Neighborhood).Include(n => n.Neighborhood.District).
                 Include(m => m.Neighborhood.District.City).Include(e => e.Tip).Include(e => e.Tip.Status).OrderByDescending(i => i.AdvId); ;
             //ModelState.Clear();
-            return View(adv.ToList());
-
+            return View(adv.ToList().ToPagedList(page , 6 ));
         }
     }
 }
