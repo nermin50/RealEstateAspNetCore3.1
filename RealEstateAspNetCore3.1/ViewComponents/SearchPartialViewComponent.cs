8d501d0d635448fa9cbdf62855b853ef2b2e0348
nermin50 <nerminsaar@gmail.com>
2021-08-28 12:37:09 +0300

    Convert SearchPartial  TO  View Components

---
+++ RealEstateAspNetCore3.1/ViewComponents/SearchPartialViewComponent.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RealEstateAspNetCore3._1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RealEstateAspNetCore3._1.ViewComponents
+{
+    public class SearchPartialViewComponent : ViewComponent
+    {
+        private readonly DataContext _db;
+
+        public SearchPartialViewComponent(DataContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+
+            return View();
+
+        }
+
+    }
+}
