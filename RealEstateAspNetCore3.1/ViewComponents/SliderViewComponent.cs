276d5c341f2dfee5f6385ce54353ff38648fc4a6
nermin50 <nerminsaar@gmail.com>
2021-08-28 13:35:22 +0300

    Convert Slider Partial View To View Component

---
+++ RealEstateAspNetCore3.1/ViewComponents/SliderViewComponent.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RealEstateAspNetCore3._1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RealEstateAspNetCore3._1.ViewComponents
+{
+    public class SliderViewComponent : ViewComponent
+    {
+        private readonly DataContext _db;
+
+        public SliderViewComponent(DataContext db)
+        {
+            _db = db;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var adv = _db.advertisements.ToList().Take(5);
+            var imgs = _db.advPhotos.ToList();
+            ViewBag.imgs = imgs;
+            return View(adv);
+
+        }
+    }
+}
